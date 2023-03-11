db95db750c1a3d5c8319558c0dca232abbad9c7d
Signal992 <127075542+Signal992@users.noreply.github.com>
2023-03-11 13:13:37 +0500

    Create simple_interest.sh

---
+++ simple_interest.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#its a sh file for the task 6 of github.
+
+echo "type the contents to be displayed without initials: "
+read nm
+read lnm
+
+fulnm="Ms/Mr $nm $lnm"
+
+echo "fullname is: $fulnm"
